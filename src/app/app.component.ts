33cc01bd364bf1617f4e827fce9c2294dc55ea9b
Matias Poshnjari <mposhnjari.exe@oraya.io>
2024-05-11 20:56:59 +0200

    added onInit for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Import Router
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'finance-manager';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    console.log('AppComponent initialized');
+    const localData = localStorage.getItem('token');
+    if (!localData) {
+      this.router.navigate(['/login']); // Navigate to login
+    }
+  }
 }
