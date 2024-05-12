97ecfe55d0bf3f3525a754b1dd158d298781f513
Fioralba Frasheri <fioralbafrasheri2002@gmail.com>
2024-05-12 16:57:29 +0200

    bb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { register } from 'module';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BalanceSheetComponent } from './components/balance-sheet/balance-sheet.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 const routes: Routes = [
   { path: '', component: BalanceSheetComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'transactions', component: TransactionsComponent}
+  // { path: 'profile', component: Yoi}
 ];
 
 @NgModule({
