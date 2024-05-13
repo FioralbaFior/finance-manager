2c8dfd2c6cd355e35a82a6b60d523901aa545912
Fioralba Frasheri <fioralbafrasheri2002@gmail.com>
2024-05-13 15:15:40 +0200

    fjdi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BalanceSheetComponent } from './components/balance-sheet/balance-sheet.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'transactions', component: TransactionsComponent}
+  { path: 'transactions', component: TransactionsComponent },
+  // { path: 'balance', component: BalanceSheetComponent }
   // { path: 'profile', component: Yoi}
 ];
 
@@ -19,4 +20,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
