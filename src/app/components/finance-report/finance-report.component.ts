2a818371b7b8f542b1ab3f12dbfb9c57d3e7b3fc
Matias Poshnjari <mposhnjari.exe@oraya.io>
2024-05-23 00:29:07 +0200

    Added more changes

--- src/app/components/finance-report/finance-report.component.ts
+++ src/app/components/finance-report/finance-report.component.ts
@@ -15,11 +15,39 @@ export class FinanceReportComponent implements OnInit {
     this.chart = new Chart('canvas', {
       type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December',
+        ],
         datasets: [
           {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Revenue',
+            data: [
+              12000, 15000, 18000, 20000, 17000, 16000, 19000, 21000, 22000,
+              23000, 24000, 25000,
+            ],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+          },
+          {
+            label: 'Expenses',
+            data: [
+              8000, 9000, 10000, 12000, 11000, 9500, 10000, 11000, 12000, 13000,
+              14000, 15000,
+            ],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1,
           },
         ],
@@ -28,6 +56,14 @@ export class FinanceReportComponent implements OnInit {
         scales: {
           y: {
             beginAtZero: true,
+          },
+        },
+        layout: {
+          padding: {
+            left: 50,
+            right: 50,
+            top: 0,
+            bottom: 0,
           },
         },
       },
